636152ac005faed69fde111fd40418e6e03f2410
Donavan Tjin <donovan__tls@hotmail.com>
2022-06-06 16:29:23 +0200

    Updates

---
+++ precache-manifest.09e3c5f6d64011fa639ba8a5c36d0737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6e3197441d7e141e20e",
+    "url": "/React-Calculator-Tjin/static/css/main.ad2f2ab0.chunk.css"
+  },
+  {
+    "revision": "c6e3197441d7e141e20e",
+    "url": "/React-Calculator-Tjin/static/js/main.c6e31974.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/React-Calculator-Tjin/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "26fb0ba8c628b846ce36",
+    "url": "/React-Calculator-Tjin/static/js/2.26fb0ba8.chunk.js"
+  },
+  {
+    "revision": "d00a7c96a4b93db5bc6b",
+    "url": "/React-Calculator-Tjin/static/js/runtime~main.d00a7c96.js"
+  },
+  {
+    "revision": "e1bf638c6dae3246dae3e56adf8b8702",
+    "url": "/React-Calculator-Tjin/index.html"
+  }
+];
